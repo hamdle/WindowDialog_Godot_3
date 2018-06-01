7eee16ab82ae4d8707330e59b82d5d53c4cd3e32
hamdle <contact@ericmarty.co>
2018-06-01 10:31:27 -0700

    Clean up containers and fix button toggle

--- Node.gd
+++ Node.gd
@@ -4,7 +4,7 @@ extends Node
 func _on_Button_pressed():
 	#var button = get_node("Container")
 	#button.visible = false
-	var dialog = get_node("Container2/WindowDialog")
+	var dialog = get_node("Container/WindowDialog")
 	dialog.popup()
 
 
@@ -14,3 +14,8 @@ func _on_ContinueButton_pressed():
 
 func _on_NewGameButton_pressed():
 	print("new game")
+
+
+func _on_WindowDialog_popup_hide():
+	var button = get_node("Container/StartButton")
+	button.pressed = false
